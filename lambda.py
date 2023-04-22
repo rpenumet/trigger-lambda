689ed4ff6a1463bf9d196c02de4c806cd90701ab
Ravi <rpenumet@amazon.com>
2023-04-22 15:05:22 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,5 +1,23 @@
+import boto3
+
 def lambda_handler(event, context):
-    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
-    return { 
-        'message' : message
-    }
+
+    sts_connection = boto3.client('sts')
+    acct_b = sts_connection.assume_role(
+        RoleArn="arn:aws:iam::593204228656:role/service-role/AWSCodePipelineServiceRole-us-east-1-trigger-lambda-function-pi",
+        RoleSessionName="cross_acct_lambda"
+    )
+    
+    ACCESS_KEY = acct_b['Credentials']['AccessKeyId']
+    SECRET_KEY = acct_b['Credentials']['SecretAccessKey']
+    SESSION_TOKEN = acct_b['Credentials']['SessionToken']
+
+    # create service client using the assumed role credentials, e.g. S3
+    client = boto3.client(
+        's3',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        aws_session_token=SESSION_TOKEN,
+    )
+
+    return "Hello from Lambda"
